b0f661c8374b7c64da4ba09526206076fc867c26
Jchn <jchn.wrk@gmail.com>
2019-03-16 23:28:18 -0400

    update

---
+++ build/precache-manifest.01d616330da7a30dbf1e694940c054d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "534c1533d19018eb5f1f",
+    "url": "/clickyhw/static/js/runtime~main.9dbbbc48.js"
+  },
+  {
+    "revision": "b7ab10506eeaaddd3abd",
+    "url": "/clickyhw/static/js/main.052c47f4.chunk.js"
+  },
+  {
+    "revision": "7c95e5acb12998c68d21",
+    "url": "/clickyhw/static/js/2.3c99c517.chunk.js"
+  },
+  {
+    "revision": "b7ab10506eeaaddd3abd",
+    "url": "/clickyhw/static/css/main.05025b4c.chunk.css"
+  },
+  {
+    "revision": "c58b8e0aa59ece5c4809322880f9864d",
+    "url": "/clickyhw/index.html"
+  }
+];
